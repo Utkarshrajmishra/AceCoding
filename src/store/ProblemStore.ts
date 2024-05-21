c09c049b0fa5646be9d313100de14a4a979175e1
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-05-22 00:41:26 +0530

    Add Problem Level in Problem Store

--- src/store/ProblemStore.ts
+++ src/store/ProblemStore.ts
@@ -1,11 +1,13 @@
 import { create } from "zustand";
 
 interface ProblemState {
-  statement: string;
-  changeStatement: (problem:string) => void;
+  problemStatement: string,
+  problemLevel:string,
+  changeStatement: (statement:string) => void;
 }
 
 export const useProblemStore = create<ProblemState>((set)=>({
-    statement:"",
-    changeStatement:(problem:string)=>set({statement:problem})
+    problemStatement:"",
+    problemLevel:"",
+    changeStatement:(statement:string)=>set({problemStatement:statement})
 }))
